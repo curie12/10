9767dd87a31705454eb69c3aaf0d8e20f8677f82
Kyuri <curie1211@sm.ac.kr>
2023-11-09 15:54:19 +0900

    실습 5

--- main.c
+++ main.c
@@ -3,10 +3,26 @@
 
 void main(void)
 {
-  char str[30] = "happy C programming";
+  FILE* fp;
+  char str[100];
+  int i;
   
-  printf("문자열\"%s\"의 길이: %i\n", str, strlen(str));
-
+  //1. OPEN FILE
+  fp = fopen("sample.txt","w");//두가지 입력 (파일명,모드) 
+  
+  //2. WRITE FILE
+  for(i=0;i<3;i++)
+  {
+      //2-1. PRINT "input a word"]
+      printf("input a word: ");
+      //2-2. SCANF A STRING
+      scanf("%s", str);
+      //2-3. fprintf()
+      fprintf(fp, "%s\n", str);
+  }
+  //3. CLOSE FILE
+  fclose(fp); 
+  
   system("PAUSE");	
   return 0;
 }
